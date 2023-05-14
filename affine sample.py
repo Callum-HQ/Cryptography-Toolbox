6c2d26844c020a70bb9a4ac1d0dc46c544b77c05
Kairos Tay <80029462+Kairos-T@users.noreply.github.com>
2023-05-14 18:53:21 +0800

    Create affine sample.py

---
+++ affine sample.py
@@ -0,0 +1,56 @@
+print("""Iranl zudjl krira dzg blng, hrodnhgngja bkzuzdhzot nizg, dnk kr nzjdlrk gnlura zohzkzkjog jg iberan ng kriran lbtob bizxjb. 
+Nizg dhninazdxjn lbjazd uniinogndxjn ujimzoba. Kzbl mrijgubg hrllrkr dnk ntndgbd ntndgbd qazotziib. 
+Uniinogndxjn lbddb uibhnabg kjzd jigazhznd ibhjd dnk. Awrohjd lbggzd awrohjd jaob onxjn. B hrokzlnogjl mzgbn dbuzno uniinogndxjn wbezgbog lraez gazdgzxjn dnonhgjd.
+Ng inr kjzd jg kzbl xjbl ojiib uraggzgra. Nizg ntng tabmzkb hjl drhzzd obgrxjn unobgzejd ng lbtozd kzd. 
+Jigazhnd nard zo hjadjd gjauzd lbddb gzohzkjog kjz jg. Ozdz mzgbn djdhzuzg gniijd lbjazd b kzbl. 
+Tabmzkb bahj bh gragra kztozddzl hrombiizd bnonbo. Gniijd habd bkzuzdhzot nozl nj.
+Nj dhninazdxjn qnizd zlunakzng uarzo qnalnogjl inr mni rahz. Gragra bg bjhgra jaob ojoh zk hjadjd.
+Xjbl ojiib uraggzgra lbddb zk onxjn. Mrijgubg bh gzohzkjog mzgbn dnluna xjzd inhgjd. 
+Gragra hrokzlnogjl ibhzozb xjzd mni nard. Ntng ojiibl oro ozdz ndg dzg blng qbhzizdzd lbtob.
+Gjauzd hjadjd zo wbh wbezgbddn uibgnb kzhgjldg. Mndgzejijl iranl dnk azdjd jigazhznd gazdgzxjn ojiib bizxjng nozl gragra.
+
+B ibhjd mndgzejijl dnk bahj oro rkzr. Bizxjng onh jiiblhrauna dzg blng azdjd ojiibl ntng. 
+Zk kzbl lbnhnobd jigazhznd lz ntng lbjazd uwbangab. Zo wbh wbezgbddn uibgnb kzhgjldg mndgzejijl awrohjd ndg uniinogndxjn nizg.
+Dnk qnizd ntng mnizg bizxjng dbtzggzd zk hrodnhgngja. Hrodnhgngja bkzuzdhzot nizg jg bizxjbl ujajd. Dnk bjtjn ibhjd mzmnaab mzgbn hrotjn. 
+Hrotjn nj hrodnxjbg bh qnizd kronh ng rkzr. Drhzzd obgrxjn unobgzejd ng lbtozd kzd ubagjaznog. 
+Zk mnizg jg gragra uangzjl mzmnaab. Kzbl zo bahj hjadjd njzdlrk xjzd mzmnaab ozew. 
+Hrllrkr dnk ntndgbd ntndgbd qazotziib uwbdniijd. 
+Mzmbljd bahj qnizd ezenokjl jg gazdgzxjn ng ntndgbd. Nj gzohzkjog gragra bizxjbl ojiib qbhzizdz habd qnalnogjl rkzr.
+Gjauzd lbddb gzohzkjog kjz jg raoban inhgjd. Lrindgzn bh qnjtzbg dnk inhgjd mndgzejijl lbggzd jiiblhrauna mnizg. 
+Lbnhnobd dnk nozl jg dnl mzmnaab bizxjng.
+
+Bizxjbl jg uraggzgra inr b kzbl driizhzgjkzo gnlura zk nj. 
+Kzhgjl qjdhn jg uibhnabg rahz ojiib uniinogndxjn. 
+Nizg ntng tabmzkb hjl drhzzd obgrxjn. Ozdi hrokzlnogjl zk mnonobgzd b hrokzlnogjl mzgbn dbuzno. 
+Ozew habd ujimzoba lbggzd ojoh dnk eibokzg izenar mrijgubg. Dbuzno ntng lz uarzo dnk. 
+Qbhzizdz lraez gnlujd zbhjizd jaob. Zlunakzng dnk njzdlrk ozdz uragb.
+Obgrxjn unobgzejd ng lbtozd kzd ubagjaznog lrognd. 
+Dzg blng ozdi djdhzuzg bkzuzdhzot ezenokjl ndg jigazhznd.
+Ojoh ujimzoba dbuzno ng iztjib jiiblhrauna.
+Azdjd qnjtzbg zo bogn lngjd kzhgjl bg gnlura hrllrkr. 
+Ireragzd ninlnogjl ozew gniijd lrindgzn ojoh oro eibokzg lbddb nozl.
+
+Qnalnogjl rkzr nj qnjtzbg uangzjl ozew zudjl hrodnxjbg ozdi. 
+Bizxjng ntng dzg blng gniijd habd bkzuzdhzot nozl nj. Mzgbn ng inr kjzd jg.
+Qnalnogjl rkzr nj qnjtzbg uangzjl ozew zudjl hrodnxjbg ozdi mni. 
+Hrokzlnogjl mzgbn dbuzno uniinogndxjn wbezgbog.
+Lbddb gnlura onh qnjtzbg ozdi uangzjl qjdhn zk mnizg.
+Kjz dbuzno ntng lz uarzo dnk. 
+Wbezgbog lraez gazdgzxjn dnonhgjd ng ongjd ng lbindjbkb qblnd bh.
+Dnk mnizg kztozddzl drkbind jg nj dnl zogntna mzgbn cjdgr.
+Dnluna azdjd zo wnokanazg tabmzkb ajgajl xjzdxjn oro gniijd.
+
+Bizxjbl qbjhzejd ujajd zo lbddb gnlura onh. 
+Azdjd dnk mjiujgbgn rkzr jg nozl eibokzg mrijgubg.
+Driizhzgjkzo ozew dzg blng hrllrkr ojiib qbhzizdz. 
+Dnl zogntna mzgbn cjdgr ntng lbtob qnalnogjl zbhjizd nj.
+Ntng bahj kzhgjl mbazjd kjzd bg hrodnhgngja iranl.
+Hjadjd ntng ojoh dhninazdxjn mzmnaab lbjazd zo bizxjbl.
+Qbhzizdz lraez gnlujd zbhjizd jaob zk mrijgubg ibhjd.
+Ibhjd mndgzejijl dnk bahj oro rkzr njzdlrk ibhzozb bg. 
+Zk kronh jigazhnd gzohzkjog bahj. 
+Gzohzkjog bjtjn zognakjl mnizg njzdlrk zo uniinogndxjn lbddb uibhnabg. 
+Mzgbn hrotjn lbjazd awrohjd bnonbo mni. 
+Bh gjauzd ntndgbd lbnhnobd uwbangab hrombiizd urdjnan. 
+Zognakjl urdjnan iranl zudjl krira dzg blng hrodnhgngja. 
+Lz ntng lbjazd uwbangab ng jigazhnd onxjn.""")
